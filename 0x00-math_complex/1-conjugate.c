811640eb07e9f7c722171c016a14317985fe0064
mglssr <iglesiasmaia6@gmail.com>
2022-04-21 09:21:34 -0700

    task 1(?)

---
+++ 0x00-math_complex/1-conjugate.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+* complex conjugate(complex c)
+* @c: complex number
+* Return: complex conjugate
+*/
+
+complex conjugate(complex c)
+{
+	c.im = c.im * -1;
+	return(c);
+}
