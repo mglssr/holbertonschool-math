e09735de71641152e6a29c63fdf5bc6c5c9feaff
mglssr <iglesiasmaia6@gmail.com>
2022-04-27 15:34:44 -0700

    ahre

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -9,15 +9,28 @@
 
 void division(complex c1, complex c2, complex *c3);
 {
-	double a_0, a_1, a_2, b_0, b_1;
+	complex c2_con;
+	double a_0, a_1, a_2, b_0, b_1, z_0, z_1, y_0 y_1;
+	double i_re, i_im;
 
 	//conjugada del denominador
-	
-	//producto del numerador por la conjugada de denom
+	c2_con = conjugate(c2);		
+	//producto del numerador (c1) por la conjugada de denom
+	a_0 = c1.re * c2_con.re;
+	a_1 = c1.re * c2_con.im;
+	b_0 = c1.im * c2_con.re;
+	b_1 = c1.im * c2_con.im;
 
-	//producto del denom por su conjugada
 
-	//cociente de real/real y real/complex
-
+	//producto del denom (c2) por su conjugada z_1 + y_0 = bc son opuestos
+	z_0 = c2.re * c2_con.re;
+	z_1 = c2.re * c2_con.im;
+	y_0 = c2.im * c2_con.re;
+	y_1 = c2.im * c2_con.re;
+	// necesito el opuesto de y_1
+	y_1 *= -1;
+	a_2 = y_1 + z_0;
+	//cociente de real/real y complex/real
+	i_re = 
 	//definir el complex c3 :D
 }
