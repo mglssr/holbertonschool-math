60ed6cef8c6aa6018bde33956ddcb983d735e63f
mglssr <iglesiasmaia6@gmail.com>
2022-04-27 14:23:00 -0700

    base

---
+++ 0x00-math_complex/7-division.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+* function that performs the division operation to complex numbers.
+* @c1: numerator complex
+* @c2: denominator complex
+* @c3: quotient complex
+*/
+
+void division(complex c1, complex c2, complex *c3);
+{
+	double a_0, a_1, a_2, b_0, b_1;
+
+	//conjugada del denominador
+	
+	//producto del numerador por la conjugada de denom
+
+	//producto del denom por su conjugada
+
+	//cociente de real/real y real/complex
+
+	//definir el complex c3 :D
+}
