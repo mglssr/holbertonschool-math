70a92f7dcb58ec3cc2a97c8b5d709c84c2227897
mglssr <iglesiasmaia6@gmail.com>
2022-06-25 16:09:52 -0700

    el file posta

---
+++ 0x02-math_integrals_and_ode/0-rectangle.c
@@ -0,0 +1,27 @@
+#include "rectangle.h"
+#include <math.h>
+
+/**
+* rectangle_method - function that computes the rectangle method to calculate
+* int[0,1] 1/ (1 + x^2)
+* @a: integration limit
+* @b: integration limit
+* @steps: number of partitions
+* Return: approximation of the integral
+*/
+
+double rectangle_method(double a, double b, int steps )
+{
+	double integral = 0, base, sum, xi; 
+	int i = 0;
+
+	while(i < steps)
+	{
+		base = (b - a)/steps;
+		xi = a + (i * base);
+		sum = 1/(1 + ((xi) * (xi))) * base;
+		integral = integral + sum;
+		i++;
+	}
+	return (integral);
+}
