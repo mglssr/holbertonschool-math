38f10ce92fb4f2444c8c3eb15f68daafa8d57db3
mglssr <iglesiasmaia6@gmail.com>
2022-07-20 18:07:38 -0700

    task 1

--- 0x04-Math_Gauss_elimination/0-gaussian_elimination.py
+++ 0x04-Math_Gauss_elimination/0-gaussian_elimination.py
@@ -4,5 +4,10 @@
 
 import numpy as np
 
+
 def gaussian_elimination(A, b):
-    return np.linalg.solve(A, b)
+    try:
+        return np.linalg.solve(A, b)
+    except:
+        print("You can't divide by zero!")
+        return None
