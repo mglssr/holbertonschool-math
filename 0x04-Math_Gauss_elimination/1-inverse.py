8bc6e22cc45192ab7ba42d664f15e00a483249d4
mglssr <iglesiasmaia6@gmail.com>
2022-07-20 19:05:09 -0700

    pep8

--- 0x04-Math_Gauss_elimination/1-inverse.py
+++ 0x04-Math_Gauss_elimination/1-inverse.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 import numpy as np
-gaussian_elimination = __import__('0-gaussian_elimination').gaussian\
-                                                        _elimination
+gaussian_elimination = \
+        __import__('0-gaussian_elimination').gaussian_elimination
 
 if __name__ == "__main__":
 
@@ -20,5 +20,5 @@ if __name__ == "__main__":
     A[3, 2] = 1
     print("The matrix A:\n", A)
     print("The inverse of the matrix A:\n", inverse(A))
-    print("The identity matrix(The matrix A x The inverse of
-                               the matrix A): \n", A @ inverse(A))
+    print("The identity matrix(The matrix A x The inverse of\
+the matrix A): \n", A @ inverse(A))
