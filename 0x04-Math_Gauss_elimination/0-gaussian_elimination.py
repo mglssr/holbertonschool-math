8ced75acc037f3fbe6a26d632cbb6a772612ce1c
mglssr <iglesiasmaia6@gmail.com>
2022-07-20 18:08:53 -0700

    task 1

--- 0x04-Math_Gauss_elimination/0-gaussian_elimination.py
+++ 0x04-Math_Gauss_elimination/0-gaussian_elimination.py
@@ -8,6 +8,6 @@ import numpy as np
 def gaussian_elimination(A, b):
     try:
         return np.linalg.solve(A, b)
-    except:
+    except LinAlgError:
         print("You can't divide by zero!")
         return None
